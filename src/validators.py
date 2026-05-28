5ca5f197bae22a7939460b34f065dc5409488d35
agrima-02 <tagrima@gmail.com>
2026-05-28 18:53:57 +0530

    Update validators.py

--- src/validators.py
+++ src/validators.py
@@ -1,16 +1,12 @@
 def validate_dataframe(df):
 
     if df.empty:
-
         raise ValueError(
             "Dataset is empty"
         )
 
-    # Count null rows
-    null_rows = df.isnull().sum().sum()
-
     print(
-        f"Null values: {null_rows}"
+        f"Rows: {len(df)}"
     )
 
     print(
